0139b26fbf92538db9630a528a2d672cd1a17a38
SulthanAuliaRahman <sulthan.aulia.tif23@polban.ac.id>
2026-01-27 10:36:16 +0700

    Faster Revalidation (untuk reporting)

--- app/(public)/mitra/page.tsx
+++ app/(public)/mitra/page.tsx
@@ -1,6 +1,8 @@
 import LandingFAQsWrapper from '@/components/server/LandingFAQs.server'
 import React from 'react'
 
+export const revalidate = 10; // second
+
 const MitraPage = () => {
   return (
     <div>MitraPage
