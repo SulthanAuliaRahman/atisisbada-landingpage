58c22e4480e9e45d9b013a476b2d54304a7e6d20
restu <restu.akbar.03.04@gmail.com>
2026-01-28 15:33:14 +0700

    fix: error dns

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,20 +1,13 @@
-
 import { PrismaClient } from "@/src/generated/prisma";
-import { PrismaPg } from "@prisma/adapter-pg";
-
-const adapter = new PrismaPg({
-  connectionString: process.env.DATABASE_URL!,
-});
 
 const globalForPrisma = global as unknown as {
   prisma: PrismaClient;
 };
 
-const prisma =
-  globalForPrisma.prisma || new PrismaClient({
-    adapter,
-  });
+const prisma = globalForPrisma.prisma ?? new PrismaClient();
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
-
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+}
 export default prisma;
+
