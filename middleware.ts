b402a82ad30ae2237af5512482a6f1789b09d1e7
restu <restu.akbar.03.04@gmail.com>
2026-02-28 17:16:40 +0700

    feat: middleware api

---
+++ middleware.ts
@@ -0,0 +1,26 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export function middleware(request: NextRequest) {
+  const { pathname } = request.nextUrl;
+
+  if (pathname.startsWith("/api/admin")) {
+    if (["GET", "HEAD", "OPTIONS"].includes(request.method)) {
+      return NextResponse.next();
+    }
+
+    const token = request.cookies.get("token")?.value;
+
+    if (!token) {
+      return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
+    }
+
+    return NextResponse.next();
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: "/api/admin/:path*",
+};
