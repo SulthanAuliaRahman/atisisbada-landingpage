c214861c510cb562adb0fae8e0f54ca675fdbb47
SulthanAuliaRahman <sulthan.aulia.tif23@polban.ac.id>
2026-03-08 17:23:31 +0700

    change db data type (jadi lebih banyak)

---
+++ prisma/migrations/20260308102049_varchar_change_to_text/migration.sql
@@ -0,0 +1,8 @@
+-- AlterTable
+ALTER TABLE "Instagram" ALTER COLUMN "deskripsi" SET DATA TYPE TEXT;
+
+-- AlterTable
+ALTER TABLE "Perkembangan" ALTER COLUMN "text" SET DATA TYPE TEXT;
+
+-- AlterTable
+ALTER TABLE "berita" ALTER COLUMN "deskripsi" SET DATA TYPE TEXT;
